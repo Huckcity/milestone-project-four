8d8f66220470584a36c8fc1d28ce3ebaa66be715
Adam Gibbons <adam.p.gibbons@gmail.com>
2019-02-10 15:39:42 +0000

    Implemented basic mysql setup with test DB

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 import os
 from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
+class Database:
+    def __init__(self):
+        host = "sql2.freesqldatabase.com"
+        user = "sql2278391"
+        password = "zW4!eF6%"
+        db = "sql2278391"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
+    def list_recipes(self):
+        self.cur.execute("SELECT * FROM recipes")
+        result = self.cur.fetchall()
+        return result
+
 @app.route("/")
 def main():
-    return render_template('index.html')
+    
+    def db_query():
+        db = Database()
+        recipes = db.list_recipes()
+        return recipes
+    
+    results = db_query()
+    
+    return render_template('index.html', result=results)
   
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
