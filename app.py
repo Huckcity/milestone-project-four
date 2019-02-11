224cdcf908f68ec291c7cb29f8661080c8b60bf3
Adam Gibbons <adam.p.gibbons@gmail.com>
2019-02-11 20:41:07 +0000

    Added basic user login/session handling, recipe adding in progress

--- app.py
+++ app.py
@@ -1,21 +1,109 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from db import *
 
 app = Flask(__name__)
+app.secret_key = "SECRETKEY"
 
-@app.route("/login")
+ ########### LOGIN PAGE ###########
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    
+    if request.method == "POST":
+    
+        uname = request.form['username']
+        password = request.form['password']
+        
+        if Database().find_user(uname, password):
+            
+            session['user'] = uname
+            return redirect(url_for('home'))
+            
+        else:
+        
+            return redirect(url_for('login'))
+    
+    else:
+    
+        return render_template('login.html')
+        
+########### LOGOUT PAGE ###########
+    
+@app.route("/logout")
+def logout():
+    
+    for key in session.keys():
+     session.pop(key)
+     
+    return redirect(url_for('main'))
+        
 
-@app.route("/signup")
+########### SIGN UP PAGE ###########
+
+@app.route("/signup", methods=['POST', 'GET'])
 def signup():
-    return render_template('signup.html')
+    
+    if request.method == "POST":
+    
+        username = request.form['username']
+        password = request.form['password']
+        
+        if Database().add_user(username, password):
+            
+            session['user'] = username
+            return redirect(url_for('home'))
+            
+        else:
+        
+            return redirect('/signup')
+            
+    else:
+    
+        return render_template('signup.html')
+        
+########### ADD RECIPES PAGE ###########
+
+@app.route("/addrecipe", methods=['POST', 'GET'])
+def addrecipe():
+    
+    if request.method == "POST":
+    
+        title = request.form['title']
+        description = request.form['description']
+        recipeGF = request.form.get['recipeGF']
+        
+        if Database().add_recipe(title, description, recipeGF, session['user']):
+
+            return redirect(url_for('recipes'))
+            
+        else:
+        
+            return redirect(url_for('addrecipe'))
+            
+    else:
+    
+        return render_template('addrecipe.html')
+    
+    
+
+########### STATIC PAGES ###########
+
+@app.route("/recipes")
+def recipes():
+    
+    recipes = Database().list_recipes()
+    
+    return render_template('recipes.html', recipes = recipes)
     
 @app.route("/about")
 def about():
     return render_template('about.html')
     
+@app.route("/home")
+def home():
+    
+    return render_template('home.html', uname = session['user'])
 
 
 @app.route("/")
--- db.py
+++ db.py
@@ -11,7 +11,51 @@ class Database:
                                    DictCursor)
         self.cur = self.con.cursor()
     
+    def add_user(self, username, password):
+        try:
+            
+            self.cur.execute("INSERT INTO users (userName, userPassword, userJoined) VALUES ('"+username+"', '"+password+"', NOW())")
+            self.con.commit()
+            result = True
+            
+        except pymysql.Error as error :
+            
+            self.con.rollback() #rollback if any exception occured
+            result = False
+            
+        return result
+        
+    def find_user(self, username, password):
+        try:
+            
+            self.cur.execute("SELECT * FROM users where userName = 'username' AND userPassword = 'password'")
+            self.con.commit()
+            result = True
+            
+        except pymysql.Error as error :
+            
+            self.con.rollback() #rollback if any exception occured
+            result = False
+            
+        return result
+    
     def list_recipes(self):
+        
         self.cur.execute("SELECT * FROM recipes")
         result = self.cur.fetchall()
+        return result
+        
+    def add_recipe(self, title, description, recipeGF, username):
+        
+        try:
+            
+            self.cur.execute("INSERT INTO recipes (recipeName, recipeDesc, recipeAdded, recipeGF, recipeAuthor) VALUES ('title', 'description', NOW(), 'recipeGF', 'username')")
+            self.con.commit()
+            result = True
+            
+        except pymysql.Error as error :
+            
+            self.con.rollback() #rollback if any exception occured
+            result = False
+            
         return result
